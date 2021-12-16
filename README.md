4d0744ad1611acffbf8939d62b64c19a28e37e3c
ONUR GÖZ <182804012@ogr.cbu.edu.tr>
2021-12-16 20:46:37 +0300

    Client project added

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# SocialWorldBackend
+# SocialWorld
  
---
+++ SocialWorld.Client/Controller/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialWorld.Client.Controller
+{
+    public class AuthController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+    }
+}
